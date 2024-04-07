d9f02f5fc2f9281ee214547dc95af252ff53d496
Олександр Гнідін <yozhegsan@gmail.com>
2024-04-07 23:37:53 +0300

    Next

--- Pages/CardInfo.cshtml.cs
+++ Pages/CardInfo.cshtml.cs
@@ -16,6 +16,8 @@ namespace Gravitas.Monitoring.Pages
 		public string UserId { get; set; } = "";
 		[BindProperty]
 		public string Result { get; set; } = "";
+		[BindProperty]
+		public bool IsActive { get; set; } = true;
 
 
 
@@ -40,27 +42,31 @@ namespace Gravitas.Monitoring.Pages
 			{
 				tc = tmp[0][5];
 				UserId = tmp[0][4];
+				IsActive = tmp[0][3] == "True" ? true : false;
 			}
 			if (db.EnterpriseNum == 1)
 			{
 				tc = tmp[0][4];
 				UserId = tmp[0][5];
+				IsActive = tmp[0][3] == "True" ? true : false;
 			}
 		}
 
 		private void WriteData()
 		{
+			string _isactive = IsActive ? "1" : "0";
 			string _tc = "";
 			string _UserId = "";
 			if (string.IsNullOrEmpty(tc)) _tc = "NULL"; else _tc = "'" + tc + "'";
 			if (string.IsNullOrEmpty(UserId)) _UserId = "NULL"; else _UserId = "'" + UserId + "'";
+
 			string sql = "";
-			if (db.EnterpriseNum == 0) sql = "update dbo.Cards set TicketContainerId=" + _tc + ", EmployeeId=" + _UserId + " where Id = '" + CardId + "'";
-			if (db.EnterpriseNum == 1) sql = "update dbo.Card set TicketContainerId=" + _tc + ", EmployeeId=" + _UserId + " where Id = '" + CardId + "'";
+			if (db.EnterpriseNum == 0) sql = "update dbo.Cards set IsActive=" + _isactive + ", TicketContainerId=" + _tc + ", EmployeeId=" + _UserId + " where Id = '" + CardId + "'";
+			if (db.EnterpriseNum == 1) sql = "update dbo.Card set IsActive=" + _isactive + ", TicketContainerId=" + _tc + ", EmployeeId=" + _UserId + " where Id = '" + CardId + "'";
 			try
 			{
 				db.SendRequestToDB(sql);
-				log.Add("User: " + User.Identity.Name + " CardEdit new data (Id: " + CardId + " TicketContainer: " + _tc + " EmployeeId: " + _UserId + ")");
+				log.Add("User: " + User.Identity.Name + " CardEdit new data (IsActive=" + _isactive + " Id: " + CardId + " TicketContainer: " + _tc + " EmployeeId: " + _UserId + ")");
 				Result = "«бережено...";
 
 			}
